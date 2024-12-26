3da428e0ee0ddfddd1def0c26b4e6b08eee345d8
id0 <ocean_ido@yahoo.com>
2024-12-26 12:32:51 +0300

    Ocean veritabanina gore duzenlendi.

--- db/insert.py
+++ db/insert.py
@@ -14,9 +14,8 @@ def coin_data_insert(coin_data, thread_num):
         indikator_values = [(coin_kodu, indikator_adi, indikator_degeri, islem_zaman_birimi, olusturma_zamani, gelen_veri_zamani)
                             for indikator_adi, indikator_degeri in coin_data.indicators.items()]
 
-        cursor.executemany("INSERT INTO IndikatorVerileri(CoinKodu,IndikatorAdi,IndikatorDegeri,IslemZamanBirimi,OlusturmaZamani,GelenVeriZamani) VALUES(?,?,?,?,?,?)",
+        cursor.executemany("INSERT INTO INDICATOR_DATA(SYMBOL_CODE, INDICATOR_NAME, INDICATOR_VALUE, INTERVAL_TYPE, INSERTED_DATE_TIME, INCOMING_DATA_DATE_TIME) VALUES(?,?,?,?,?,?)",
                            indikator_values)
-
         cursor.commit()
 
     except Exception as e:
--- db/select.py
+++ db/select.py
@@ -5,7 +5,7 @@ def withdraw_coins():
     cursor = connection.cursor_function()
 
     try:
-        cursor.execute("SELECT CoinKodu FROM Coins.dbo.CoinListesi WHERE AktifDurumu = 1")
+        cursor.execute("SELECT CODE FROM SYMBOL WHERE ACTIVE = 1")
         coin_listesi = cursor.fetchall()
         return coin_listesi
     except Exception as e:
